bae9fa94684b9d36a5ac0b2bea0b977a90418a08
nasutance <n.nasuti@gmail.com>
2025-01-04 17:01:00 +0100

    Modèle Type + migration + seeders

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Type;
+
+class TypeSeeder extends Seeder
+{
+  public function run(): void
+  {
+    Type::truncate();
+    $types =
+    [
+      ['type'=>'comédien'],
+      ['type'=>'scénographe'],
+      ['type'=>'auteur'],
+    ];
+    DB::table('type')->insert($types);
+  }
+}
