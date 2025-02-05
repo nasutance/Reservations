4e81d87ef19fa229553586c066e49e81620d893a
nasutance <n.nasuti@gmail.com>
2025-02-05 17:22:14 +0100

    Authentification by token

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,33 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ArtistController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
-Route::get('/user', function (Request $request) {
-  return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', function (Request $request) {
+  $request->validate([
+    'email' => 'required|email',
+    'password' => 'required',
+  ]);
+  $user = User::where('email', $request->email)->first();
+  if (!$user || !Hash::check($request->password, $user->password)) {
+    return response()->json(['message' => 'Invalid credentials'], 401);
+  }
+  $token = $user->createToken('api-token')->plainTextToken;
+  return response()->json(['token' => $token], 200);
+});
 
-Route::apiResource('artists', ArtistController::class)
-->middleware('auth.basic');
+Route::middleware('auth:sanctum')->group(function () {
+  Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+    return ['token' => $token->plainTextToken];
+  });
+  Route::get('/user', function (Request $request) {
+    return $request->user();
+  });
+});
+//Route::apiResource('artists', ArtistApiController::class)
+//	->middleware('auth.basic');
+Route::middleware('auth:sanctum')->group(function () {
+  Route::apiResource('artists', ArtistController::class);
+});
