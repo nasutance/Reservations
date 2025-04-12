8319f01cc3318ec30e865d5203b0973180f49b50
nasutance <n.nasuti@gmail.com>
2025-04-12 18:37:31 +0200

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        },
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
