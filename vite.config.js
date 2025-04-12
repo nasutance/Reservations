43320db171e9dcfc55d872529dda1580aa083ca3
nasutance <n.nasuti@gmail.com>
2025-04-12 19:25:51 +0200

    debug verify email

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     base: '/build/',
+    define: {
+    'process.env': {},
+},
     build: {
         outDir: 'public/build',
         manifest: true,
